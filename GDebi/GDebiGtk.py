BodhiDev/gDebi

commit b90972c84003fbf161f7b2818aff88b6fc337496

Flatten away python2 polyfills

GDebi/GDebiGtk.py
@@ -32,12 +32,6 @@ import sys
 import time
 import tempfile
 import threading
-# py3 compat
-try:
-    from urllib import url2pathname
-    url2pathname  # pyflakes
-except ImportError:
-    from urllib.request import url2pathname
 
 import gi
 gi.require_version("Gtk", "3.0")
@@ -55,7 +49,7 @@ from gettext import gettext as _
 
 from .DebPackage import DebPackage
 from .SimpleGtkbuilderApp import SimpleGtkbuilderApp
-from .GDebiCommon import GDebiCommon, utf8
+from .GDebiCommon import GDebiCommon
 
 # the timeout when the termial is expanded if no activity from dpkg
 # is happening
@@ -253,7 +247,7 @@ class GDebiGtk(SimpleGtkbuilderApp, GDebiCommon):
         buf = self.textview_description.get_buffer()
         try:
             long_desc = ""
-            raw_desc = utf8(self._deb["Description"]).split("\n")
+            raw_desc = self._deb["Description"].split("\n")
             # append a newline to the summary in the first line
             summary = raw_desc[0]
             raw_desc[0] = ""
@@ -290,9 +284,9 @@ class GDebiGtk(SimpleGtkbuilderApp, GDebiCommon):
 
         # set various status bits
         self.label_version.set_text(self._deb["Version"])
-        self.label_maintainer.set_text(utf8(self._deb["Maintainer"]))
+        self.label_maintainer.set_text(self._deb["Maintainer"])
         self.label_priority.set_text(self._deb["Priority"])
-        self.label_section.set_text(utf8(self._deb["Section"]))
+        self.label_section.set_text(self._deb["Section"])
         self.label_size.set_text(self._deb["Installed-Size"] + " KiB")
 
         # set file list

GDebi/GDebiCommon.py
@@ -25,6 +25,7 @@ import logging
 import os
 import sys
 from mimetypes import guess_type
+from gettext import gettext as _
 
 import apt_pkg
 from apt.cache import Cache
@@ -33,28 +34,6 @@ from .DebPackage import (
     ClickPackage,
     DebPackage,
 )
-
-
-if sys.version_info[0] == 3:
-    from gettext import gettext as _
-    def py3utf8(s):
-        return s
-    utf8 = py3utf8
-    unicode = str
-else:
-    def _(str):
-        return utf8(gettext.gettext(str))
-
-
-    def py2utf8(str):
-        if isinstance(str, unicode):
-            return str
-        try:
-            return unicode(str, 'UTF-8')
-        except:
-            # assume latin1 as fallback
-            return unicode(str, 'latin1')
-    utf8 = py2utf8
 
 
 class GDebiCommon(object):

gdebi
@@ -22,12 +22,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# silly py3 compat, py3.3 should make this unneeded
-try:
-    unicode
-except NameError:
-    unicode = lambda *args: args[0]
-
 import sys
 import os.path
 import gettext
@@ -47,27 +41,27 @@ if __name__ == "__main__":
     gettext.bindtextdomain(localesApp, localesDir)
     gettext.textdomain(localesApp)
 
-    usage = unicode(_("usage: %prog [options] filename\n"
-                      "For a graphical version run gdebi-gtk\n"),"UTF-8")
+    usage = _("usage: %prog [options] filename\n"
+              "For a graphical version run gdebi-gtk\n")
     parser = OptionParser(usage=usage, version = VERSION)
     parser.add_option("-n", "--non-interactive",
                       action="store_true", dest="non_interactive",
                       default=False,
-                      help=unicode(_("Run non-interactive (dangerous!)"),"UTF-8"))
+                      help=_("Run non-interactive (dangerous!)"))
     parser.add_option("-o", "--option",
                       action="append", dest="apt_opts",
                       default=[],
-                      help=unicode(_("Set an APT configuration option"),"UTF-8"))
+                      help=_("Set an APT configuration option"))
     parser.add_option("-q", "--quiet",
                       action="store_true", dest="quiet",
                       default=False,
-                      help=unicode(_("Do not show progress information"),"UTF-8"))
+                      help=_("Do not show progress information"))
     parser.add_option("--apt-line",
                       action="store_true", dest="apt_line",
                       default=False,
-                      help=unicode(_("Simulate only and print a apt-get install compatible line to stderr"), "UTF-8"))
+                      help=_("Simulate only and print a apt-get install compatible line to stderr"))
     parser.add_option("--root", dest="rootdir", default="/",
-                      help=unicode(_("Use alternative root dir"), "UTF-8"))
+                      help=_("Use alternative root dir"))
     (options, args) = parser.parse_args()
 
     if len(args) == 0:

gdebi-gtk
@@ -22,12 +22,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
-# silly py3 compat, py3.3 should make this unneeded
-try:
-    unicode
-except NameError:
-    unicode = lambda *args: args[0]
-
 import sys
 
 from optparse import OptionParser
@@ -51,15 +45,15 @@ if __name__ == "__main__":
     parser.add_option("-n", "--non-interactive",
                       action="store_true", dest="non_interactive",
                       default=False,
-                      help=unicode(_("Run non-interactive (dangerous!)"),"UTF-8"))
+                      help=_("Run non-interactive (dangerous!)"))
     parser.add_option("--auto-close", "",
                       action="store_true", default=False,
-                      help=unicode(_("Auto close when the install is finished"),"UTF-8"))
+                      help=_("Auto close when the install is finished"))
     parser.add_option("--datadir", "", default="",
-                      help=unicode(_("Use alternative datadir"),"UTF_8"))
+                      help=_("Use alternative datadir"))
     parser.add_option("-r", "--remove", default="False",
                       action="store_true", dest="remove",
-                      help=unicode(_("Remove package"),"UTF-8"))
+                      help=_("Remove package"))
     (options, args) = parser.parse_args()
 
     if options.datadir:

commit 59304d01d4f640f7cb2cf1fb03d92f3332768360

Re-add url2pathname import
Fixes: b90972c84003fbf161f7b2818aff88b6fc337496

GDebi/GDebiGtk.py
@@ -32,6 +32,7 @@ import sys
 import time
 import tempfile
 import threading
+from urllib.request import url2pathname
 
 import gi
 gi.require_version("Gtk", "3.0")